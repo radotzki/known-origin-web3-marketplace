cee374d65be9de417a7fb88cc256b8bb80eebb33
James Morgan <james.morgan.e@googlemail.com>
2018-06-13 15:22:28 +0100

    Chopping events

--- src/store/modules/assets.js
+++ src/store/modules/assets.js
@@ -12,8 +12,7 @@ const contractStateModule = {
     assets: null,
     assetsByEditions: null,
     assetsByArtistCode: null,
-    editionSummary: null,
-    events: []
+    editionSummary: null
   },
   getters: {
     assetsForEdition: (state) => (edition) => {
@@ -181,10 +180,6 @@ const contractStateModule = {
       Vue.set(state, 'assetsByArtistCode', assetsByArtistCode);
       Vue.set(state, 'editionSummary', editionSummary);
     },
-    [mutations.ADD_EVENT](state, anEvent) {
-      console.log(anEvent);
-      Vue.set(state, 'events', [anEvent]);
-    }
   },
   actions: {
     [actions.GET_ALL_ASSETS]({commit, dispatch, state, rootState}) {
@@ -356,39 +351,7 @@ const contractStateModule = {
               return _.without(assets, null);
             })
             .then(populateTokenUriData)
-            .then(bindAssetsToStore)
-            .then(() => dispatch(actions.EVENT_STREAM));
-        });
-    },
-    [actions.EVENT_STREAM]({commit, dispatch, state, rootState}) {
-      return rootState.KnownOriginDigitalAsset.deployed()
-        .then((contract) => {
-
-          let purchaseWithEtherEvent = contract.PurchasedWithEther({}, {
-            fromBlock: 0,
-            toBlock: 'latest' // wait until event comes through
-          });
-
-          let purchaseWithFiatEvent = contract.PurchasedWithFiat({}, {
-            fromBlock: 0,
-            toBlock: 'latest' // wait until event comes through
-          });
-
-          purchaseWithEtherEvent.watch(function (error, anEvent) {
-            if (!error) {
-              commit(mutations.ADD_EVENT, anEvent);
-            } else {
-              console.log('Failure', error);
-            }
-          });
-
-          purchaseWithFiatEvent.watch(function (error, anEvent) {
-            if (!error) {
-              commit(mutations.ADD_EVENT, anEvent);
-            } else {
-              console.log('Failure', error);
-            }
-          });
+            .then(bindAssetsToStore);
         });
     }
   }
