077cee6e0a4b646909f394ce884acc48e5259f71
Andy Gray <andy@whatsthescore.co.uk>
2018-09-25 09:47:10 +0100

    new components

---
+++ src/components/ui-controls/artist/ArtistPanel.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="shadow-sm bg-white p-4 mb-5">
+    <artist-short-bio :artist="artist"></artist-short-bio>
+
+    <div class="text-center mb-4">
+      <a class="btn btn-sm btn-twitter" v-if="artist.twitter" :href="`https://twitter.com/${artist.twitter}`" target="_blank">Twitter</a>
+      <a class="btn btn-sm btn-telegram" href="https://t.me/knownorigin_io">Telegram</a>
+    </div>
+
+    <h6>Artist Bio</h6>
+    <p class="bio">
+      {{ artist.strapline }}
+    </p>
+  </div>
+</template>
+
+<script>
+  import ArtistShortBio from './ArtistShortBio';
+
+  export default {
+    name: 'artistPanel',
+    components: {ArtistShortBio},
+    props: ['artist']
+  };
+</script>
+
+<style scoped lang="scss">
+  @import '../../../ko-colours.scss';
+
+  .bio {
+    font-size: 0.75rem;
+    color: $body-color;
+  }
+
+  h6 {
+    color: $body-color;
+  }
+
+  .btn-twitter {
+    background-color: #20a2eb;
+    color: $body-bg;
+  }
+
+  .btn-telegram {
+    background-color: #0088cc;
+    color: $body-bg;
+  }
+
+  /* mobile only */
+  @media screen and (max-width: 767px) {
+    img {
+      max-height: 75px;
+    }
+  }
+</style>
