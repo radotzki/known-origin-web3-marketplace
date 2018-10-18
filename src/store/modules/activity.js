0d10c9ee995c35ff075b5f2082d7941e5ffee645
Andy Gray <andy@whatsthescore.co.uk>
2018-10-18 13:39:48 +0100

    review comment

--- src/store/modules/activity.js
+++ src/store/modules/activity.js
@@ -5,8 +5,7 @@ import * as mutations from '../mutation';
 const activityStateModule = {
   namespaced: true,
   state: {
-    activity: [],
-    activityStarted: false,
+    activity: []
   },
   getters: {},
   mutations: {
@@ -17,29 +16,28 @@ const activityStateModule = {
   },
   actions: {
     [actions.ACTIVITY]: function ({commit, dispatch, state, rootState}) {
-      if (!state.activityStarted) {
-        rootState.KnownOriginDigitalAssetV2.deployed()
-          .then((contract) => {
 
-            let mintedEvent = contract.Minted({}, {
-              fromBlock: rootState.currentNetwork === 'Main' ? rootState.KnownOriginDigitalAssetV2MainBlockNumber : 0,
-              toBlock: 'latest' // wait until event comes through
-            });
+      rootState.KnownOriginDigitalAssetV2.deployed()
+        .then((contract) => {
 
-            mintedEvent.get(function (error, events) {
-              if (!error) {
-                commit(mutations.SET_ACTIVITY, events);
-
-                const editionNumbers = _.map(events, (e) => parseInt(e.args._editionNumber.toString()));
-
-                dispatch(`kodaV2/${actions.LOAD_EDITIONS}`, editionNumbers, {root: true});
-              } else {
-                console.log('Failure', error);
-                mintedEvent.stopWatching();
-              }
-            });
+          let mintedEvent = contract.Minted({}, {
+            fromBlock: rootState.currentNetwork === 'Main' ? rootState.KnownOriginDigitalAssetV2MainBlockNumber : 0,
+            toBlock: 'latest' // wait until event comes through
           });
-      }
+
+          mintedEvent.get(function (error, events) {
+            if (!error) {
+              commit(mutations.SET_ACTIVITY, events);
+
+              const editionNumbers = _.map(events, (e) => parseInt(e.args._editionNumber.toString()));
+
+              dispatch(`kodaV2/${actions.LOAD_EDITIONS}`, editionNumbers, {root: true});
+            } else {
+              console.log('Failure', error);
+              mintedEvent.stopWatching();
+            }
+          });
+        });
     },
   }
 };
