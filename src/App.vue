7a11e40c31afb9396da4a71dbbcbe8e1fc193fff
James Morgan <james.morgan.e@googlemail.com>
2018-10-25 12:35:31 +0100

    Adding new mailchimp form

--- src/App.vue
+++ src/App.vue
@@ -85,12 +85,13 @@
           <div class="col">
             <!-- Begin Mailchimp Signup Form -->
             <div id="mc_embed_signup">
-              <form action="https://knownorigin.us19.list-manage.com/subscribe/post?u=84b0312927af7712ac2e6dd5a&amp;id=70433f1407" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+              <form action="https://knownorigin.us19.list-manage.com/subscribe/post?u=84b0312927af7712ac2e6dd5a&amp;id=ebee270c72" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                 <div id="mc_embed_signup_scroll">
-                  <input type="email" value="" name="EMAIL" class="email form-control" id="mce-EMAIL" placeholder="Subscribe to The Origin Weekly..." required>
+                  <label for="mce-EMAIL">Subscribe to our mailing list</label>
+                  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
                   <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                  <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_84b0312927af7712ac2e6dd5a_70433f1407" tabindex="-1" value=""></div>
-                  <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button btn"></div>
+                  <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_84b0312927af7712ac2e6dd5a_ebee270c72" tabindex="-1" value=""></div>
+                  <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
                 </div>
               </form>
             </div>
