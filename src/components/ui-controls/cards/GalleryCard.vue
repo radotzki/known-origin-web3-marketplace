bc5c94b5dc777e2733adb82ca7a2d0c1dd9247d2
Andy Gray <andy@whatsthescore.co.uk>
2018-11-04 12:41:59 +0000

    consistent footer

--- src/components/ui-controls/cards/GalleryCard.vue
+++ src/components/ui-controls/cards/GalleryCard.vue
@@ -1,30 +1,41 @@
 <template>
   <div class="card shadow-sm">
-    <router-link class="card-target"
-                 :to="{ name: 'confirmPurchase', params: { artistAccount: edition.artistAccount, editionNumber: edition.edition }}">
+    <router-link class="card-target" :to="{ name: 'confirmPurchase', params: { artistAccount: edition.artistAccount, editionNumber: edition.edition }}">
       <img class="card-img-top" :src="edition.lowResImg" :id="editionNumber"/>
-      <div class="high-res">
-        <high-res-label :high-res-available="edition.highResAvailable"></high-res-label>
-      </div>
-      <div class="card-body">
-        <p class="card-title"><creative-challenge-label :attributes="edition.attributes"></creative-challenge-label> {{ edition.name }}</p>
-      </div>
-      <div class="card-footer bg-white no-top-border">
-        <div class="row">
-          <div class="col"><price-in-eth :value="edition.priceInEther"></price-in-eth></div>
-          <div class="col text-right">
-            <availability :total-available="edition.totalAvailable" :total-supply="edition.totalSupply"></availability>
-          </div>
+    </router-link>
+    <div class="high-res">
+      <high-res-label :high-res-available="edition.highResAvailable"></high-res-label>
+    </div>
+    <div class="card-body">
+      <router-link class="card-target" :to="{ name: 'confirmPurchase', params: { artistAccount: edition.artistAccount, editionNumber: edition.edition }}">
+        <p class="card-title">
+          <creative-challenge-label :attributes="edition.attributes"></creative-challenge-label>
+          {{ edition.name }}
+        </p>
+      </router-link>
+    </div>
+    <div class="card-footer bg-white no-top-border">
+      <div class="row mb-3">
+        <div class="col">
+          <router-link class="card-target" :to="{ name: 'confirmPurchase', params: { artistAccount: edition.artistAccount, editionNumber: edition.edition }}">
+            <p class="card-text">
+              <price-in-eth :value="edition.priceInEther"></price-in-eth>
+            </p>
+          </router-link>
+        </div>
+        <div class="col text-right">
+          <router-link class="card-target" :to="{ name: 'confirmPurchase', params: { artistAccount: edition.artistAccount, editionNumber: edition.edition }}">
+            <p class="card-text">
+              <availability :total-available="edition.totalAvailable" :total-supply="edition.totalSupply"></availability>
+            </p>
+          </router-link>
         </div>
       </div>
-    </router-link>
-
-    <router-link :to="{ name: 'artist-v2', params: { artistAccount: edition.artistAccount } }" class="floating-artist-link">
-      <div class="card-footer bg-white no-top-border">
+      <router-link :to="{ name: 'artist-v2', params: { artistAccount: edition.artistAccount } }" class="floating-artist-link">
         <img :src="findArtistsForAddress(edition.artistAccount).img" class="artist-avatar"/>
         <a class="pl-1 artist-name">{{ findArtistsForAddress(edition.artistAccount).name | truncate(18) }}</a>
-      </div>
-    </router-link>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -35,7 +46,7 @@
   import ClickableAddress from '../generic/ClickableAddress';
   import Availability from '../v2/Availability';
   import HighResLabel from '../generic/HighResLabel';
-  import CreativeChallengeLabel from "../../ui-controls/generic/CreativeChallengeLabel";
+  import CreativeChallengeLabel from '../../ui-controls/generic/CreativeChallengeLabel';
   import PriceInEth from '../generic/PriceInEth';
 
   export default {
@@ -53,9 +64,7 @@
         'findArtistsForAddress'
       ])
     },
-    methods: {
-
-    }
+    methods: {}
   };
 </script>
 
