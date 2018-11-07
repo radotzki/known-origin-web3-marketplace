8c5ce1d20f9231d36a5461a5a1785629c52480cf
James Morgan <james.morgan.e@googlemail.com>
2018-11-07 21:51:21 +0000

    Minor tweak

---
+++ src/components/ui-controls/v2/RemainingCount.vue
@@ -0,0 +1,23 @@
+<template>
+  <span v-if="availability == 0">sold out</span>
+  <span v-else>
+    {{availability}} remaining
+  </span>
+</template>
+
+<script>
+  export default {
+    name: 'remainingCount',
+    props: [
+      'totalAvailable',
+      'totalSupply'
+    ],
+    computed: {
+      availability: function () {
+        return this.totalAvailable - this.totalSupply;
+      }
+    }
+  };
+</script>
+
+<style scoped lang="scss"></style>
