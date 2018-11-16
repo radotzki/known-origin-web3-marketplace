a9417858e76d908783b374fbae777c2864f4b9f8
James Morgan <james.morgan.e@googlemail.com>
2018-11-16 16:44:13 +0000

    Adding labels

---
+++ src/components/ui-controls/generic/ERC721Badge.vue
@@ -0,0 +1,22 @@
+<template>
+     <span class="badge badge-pill badge-extra-data" style="height: 20px">
+       <a href="http://erc721.org" target="_blank">
+         <font-awesome-icon :icon="['fab', 'ethereum']"></font-awesome-icon> ERC721
+       </a>
+    </span>
+</template>
+
+<script>
+  import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+
+  export default {
+    name: 'ERC721Badge',
+    components: {
+      FontAwesomeIcon
+    },
+  };
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/ui-controls/generic/IpfsBadge.vue
@@ -0,0 +1,19 @@
+<template>
+     <span
+       class="badge badge-pill badge-extra-data"
+       v-if="edition.tokenUri" style="height: 20px">
+      <img src="../../../../static/ipfs-logo.png" style="height: 14px"/> <a :href="edition.tokenUri" target="_blank">IPFS</a>
+    </span>
+</template>
+
+<script>
+
+  export default {
+    name: 'ipfsBadge',
+    props: ['edition']
+  };
+</script>
+
+<style scoped lang="scss">
+
+</style>
