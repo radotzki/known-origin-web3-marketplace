72f193e291e7a1ee5e7537c4ffe8975b088da5c0
Andy Gray <andy@whatsthescore.co.uk>
2018-11-16 20:55:19 +0000

    tweaks

--- src/components/ui-controls/generic/ERC721Badge.vue
+++ src/components/ui-controls/generic/ERC721Badge.vue
@@ -1,6 +1,6 @@
 <template>
      <span class="badge badge-pill badge-extra-data">
-       <a href="https://erc721.org" target="_blank">
+       <a href="https://erc721validator.org/?address=0xfbeef911dc5821886e1dda71586d90ed28174b7d" target="_blank">
          <font-awesome-icon :icon="['fab', 'ethereum']"></font-awesome-icon> ERC721
        </a>
     </span>
