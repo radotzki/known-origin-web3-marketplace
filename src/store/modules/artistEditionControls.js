c3348aebcc3afa60950ae8cd683a4c348e20ee76
James Morgan <james.morgan.e@googlemail.com>
2019-01-27 17:07:50 +0000

    Bug fix for gifts and p4ice changes

--- src/store/modules/artistEditionControls.js
+++ src/store/modules/artistEditionControls.js
@@ -26,28 +26,37 @@ const artistEditionControlsStateModule = {
     [actions.UPDATE_EDITION_PRICE]: async function ({commit, dispatch, state, getters, rootState}, {edition, value}) {
       const controls = await rootState.ArtistEditionControls.deployed();
       const account = rootState.account;
-      const promise = new Promise();
-      controls.updateEditionPrice(edition.edition, value, {from: account})
-        .on('transactionHash', hash => {
-          promise.resolve(hash);
-        })
-        .finally(async () => {
-          dispatch(`kodaV2/${actions.REFRESH_AND_LOAD_INDIVIDUAL_EDITION}`, {editionNumber: edition.edition}, {root: true});
-        });
-      return promise;
+
+      return new Promise((resolve, reject) => {
+        controls.updateEditionPrice(edition.edition, value, {from: account})
+          .on('transactionHash', hash => {
+            console.log("transactionHash", hash);
+            resolve(hash);
+          })
+          .finally(async () => {
+            dispatch(`kodaV2/${actions.REFRESH_AND_LOAD_INDIVIDUAL_EDITION}`, {editionNumber: edition.edition}, {root: true});
+          })
+          .catch((e) => {
+            reject(e);
+          });
+      });
     },
     [actions.GIFT_EDITION]: async function ({commit, dispatch, state, getters, rootState}, {receiver, edition}) {
       const controls = await rootState.ArtistEditionControls.deployed();
       const account = rootState.account;
-      const promise = new Promise();
-      controls.gift(receiver, edition.edition, {from: account})
-        .on('transactionHash', hash => {
-          promise.resolve(hash);
-        })
-        .finally(async () => {
-          dispatch(`kodaV2/${actions.REFRESH_AND_LOAD_INDIVIDUAL_EDITION}`, {editionNumber: edition.edition}, {root: true});
-        });
-      return promise;
+      return new Promise((resolve, reject) => {
+        controls.gift(receiver, edition.edition, {from: account})
+          .on('transactionHash', hash => {
+            console.log("transactionHash", hash);
+            resolve(hash);
+          })
+          .finally(async () => {
+            dispatch(`kodaV2/${actions.REFRESH_AND_LOAD_INDIVIDUAL_EDITION}`, {editionNumber: edition.edition}, {root: true});
+          })
+          .catch((e) => {
+            reject(e);
+          });
+      });
     },
   }
 };
