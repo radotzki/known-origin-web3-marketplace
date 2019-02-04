c8f640636b0b753ddacdb310b6c64e4cf9700431
James Morgan <james.morgan.e@googlemail.com>
2019-02-04 09:20:23 +0000

    Adding back link to BR

--- src/App.vue
+++ src/App.vue
@@ -124,6 +124,12 @@
           </div>
         </div>
         <div class="row mt-2 mb-2">
+          <div class="col text-center text-white small">
+            Built by <a href="http://blockrocket.tech">BlockRocket.tech</a>
+            using <font-awesome-icon :icon="['fab', 'ethereum']"></font-awesome-icon> & IPFS
+          </div>
+        </div>
+        <div class="row mt-2 mb-2">
           <div class="col text-center text-white">
             <current-network class="small"></current-network>
           </div>
